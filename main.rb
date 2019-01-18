3c87bf626635313645028b71e3e63f406a19f3e4
Anthony Icuspit <anthony.icuspit@simplybusiness.com>
2019-01-18 15:08:13 -0500

    testing adi_code_changes

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
-require 'fake_project_dependency'
+#require 'fake_project_dependency'
+require_relative '../fake_project_dependency/lib/fake_project_dependency.rb'
 
 puts FakeProjectDependency.hello
 puts FakeProjectDependency.a_wild_method_appeared
+puts FakeProjectDependency.tony_method
