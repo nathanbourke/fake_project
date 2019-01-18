334512cb79759120bc887b227efc19f7b190a61e
Chris Grandoit <christopher.grandoit@simplybusiness.com>
2019-01-18 15:07:51 -0500

    update sha

--- Gemfile
+++ Gemfile
@@ -1,1 +1,1 @@
-gem 'fake_project_dependency', git: 'git@github.com:nathanbourke/fake_project_dependency.git'
+gem 'fake_project_dependency', git: 'git@github.com:nathanbourke/fake_project_dependency.git', branch: 'cg_code_changes'
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
-require 'fake_project_dependency'
+require_relative '../fake_project_dependency/lib/fake_project_dependency.rb'
 
 puts FakeProjectDependency.hello
 puts FakeProjectDependency.a_wild_method_appeared
+puts FakeProjectDependency.leos_method
